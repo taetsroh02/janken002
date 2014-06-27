749ca9d84c626f814d6fadab43c4914fab933ee9
kotepe <kotepe@bizankomumakku10-no-MacBook-Air.local>
2014-06-27 11:50:32 +0900

    Initial Commit

---
+++ if sentence002/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  if sentence002
+//
+//  Created by kotepe on 2014/06/27.
+//  Copyright (c) 2014年 kotepe factory. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
